ac7031507700e0d6bcdc3d6ccd47cab28a375cd0
Pavel Dubov <PADking888@yandex.ru>
2021-09-12 09:55:07 +0300

    feat: Create "1M" relation with data-migration

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 from .models import Pokemon, PokemonEntity
 
 
-models_for_admin_interface = (Pokemon, PokemonEntity, )
+models_for_admin_interface = (
+    Pokemon,
+    PokemonEntity,
+
+)
+
 admin.site.register(models_for_admin_interface)
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -14,8 +14,13 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     lat = models.FloatField()
     lon = models.FloatField()
 
     def __str__(self):
-        return super().__str__()
+        human_readable_repr = (
+            f'{self.pokemon.title} находится в точке: '
+            f'{self.lat}, {self.lon}'
+        )
+        return human_readable_repr
