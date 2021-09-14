1b0b1c024b4a45147b71237e6e41b1f75b4fe257
Pavel Dubov <PADking888@yandex.ru>
2021-09-14 14:46:08 +0300

    feat: Add two fields in `Pokemon` model for translate title of pokemon

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,6 +5,8 @@ from .custom_storage import OverwriteStorage
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200, null=True)
+    title_jp = models.CharField(max_length=200, null=True)
     description = models.TextField(blank=False, null=True)
     image = models.ImageField(max_length=200,
                               null=True,
