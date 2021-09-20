1dccedf38a664ea07e1bd27020b8990e03522af0
Pavel Dubov <PADking888@yandex.ru>
2021-09-20 12:58:57 +0300

    feat: Display the order of evolution of the pokemons on the side page

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.db.models import F
+from django.db.models import F, Value as V
+from django.db.models.functions import Concat
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
@@ -93,30 +94,63 @@ def show_pokemon(request, pokemon_id):
     if isinstance(pokemons_kind, str):
         return HttpResponseNotFound(f'<h1>{pokemons_kind}</h1>')
 
+    pokemon = {}
+    absolute_uri = f'{request.build_absolute_uri(settings.MEDIA_URL)}'
+
+    about_pokemon_kind = {
+        'title_ru': pokemons_kind.title,
+        'title_en': pokemons_kind.title_en,
+        'title_jp': pokemons_kind.title_jp,
+        'description': pokemons_kind.description,
+        'img_url': f'{absolute_uri}{pokemons_kind.image}'
+    }
+    pokemon.update(about_pokemon_kind)
+
+    previous_evolution_of_pokemon = pokemons_kind.previous_evolution
+    if previous_evolution_of_pokemon:
+        about_pokemons_ancestor = {
+            'previous_evolution': {
+                'pokemon_id': previous_evolution_of_pokemon.id,
+                'title_ru': previous_evolution_of_pokemon.title,
+                'img_url': f'{absolute_uri}{previous_evolution_of_pokemon.image}'
+            }
+        }
+        pokemon.update(about_pokemons_ancestor)
+
+    try:
+        next_evolution_of_pokemon = (pokemons_kind.next_evolutions
+                                     .annotate(pokemon_id=F('id'),
+                                               title_ru=F('title'),
+                                               img_url=Concat(V(f'{absolute_uri}'),
+                                               'image'))
+                                     .values('pokemon_id',
+                                             'title_ru',
+                                             'img_url')
+                                     .get())
+    except Pokemon.DoesNotExist:  # Нет потомка у покемона
+        pass
+    else:
+        about_pokemons_descendant = {
+            'next_evolution': {
+                'pokemon_id': next_evolution_of_pokemon['pokemon_id'],
+                'title_ru': next_evolution_of_pokemon['title_ru'],
+                'img_url': next_evolution_of_pokemon['img_url']
+            }
+        }
+        pokemon.update(about_pokemons_descendant)
+
     pokemons_entities = (pokemons_kind.pokemonentity_set.all()
                          .select_related('pokemon')
-                         .annotate(title_ru=F('pokemon__title'),
-                                   title_en=F('pokemon__title_en'),
-                                   title_jp=F('pokemon__title_jp'),
-                                   description=F('pokemon__description'),
-                                   img_url=F('pokemon__image'))
-                         .values('pokemon_id', 'title_ru',
-                                 'title_en', 'title_jp',
-                                 'description', 'img_url',
-                                 'lat', 'lon'))
+                         .annotate(img_url=Concat(V(f'{absolute_uri}'),
+                                   'pokemon__image'))
+                         .values('img_url', 'lat', 'lon'))
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in pokemons_entities:
-        uri = request.build_absolute_uri(
-            f'{settings.MEDIA_URL}'
-            f"{pokemon_entity['img_url']}"
-        )
-        pokemon_entity['img_url'] = uri
-
         add_pokemon(folium_map, pokemon_entity['lat'],
                     pokemon_entity['lon'], pokemon_entity['img_url'])
 
     return render(request, 'pokemon.html', context={
         'map': folium_map._repr_html_(),
-        'pokemon': pokemon_entity
+        'pokemon': pokemon
     })
