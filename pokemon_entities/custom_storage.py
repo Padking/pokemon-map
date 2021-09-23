3777d4a38ebdf7f4303a5a484529b45e07a4b5ab
Pavel Dubov <PADking888@yandex.ru>
2021-09-23 13:39:05 +0300

    fix: Remove misleading comments

--- pokemon_entities/custom_storage.py
+++ pokemon_entities/custom_storage.py
@@ -8,7 +8,6 @@ class OverwriteStorage(FileSystemStorage):
     """Переопределяет поведение `FileSystemStorage`."""
 
     def get_available_name(self, name, max_length=None):
-        """Удаляет, различающиеся наименованием, картинки."""
         if self.exists(name):
             os.remove(os.path.join(settings.MEDIA_ROOT, name))
         return name
